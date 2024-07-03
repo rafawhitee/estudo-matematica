ca00d957513d6815b84f9486ae51951028142f94
DESKTOP-VI5VDF9\rafael <rafaelnunes.inf@gmail.com>
2024-07-03 01:34:51 -0300

    no message

--- geometria_analitica/001-distancia-pontos/basico.py
+++ geometria_analitica/001-distancia-pontos/basico.py
@@ -1,12 +1,11 @@
 import numpy as np
-import sympy as sp
 import matplotlib.pyplot as plt
-from utils import distancia_euclidiana, equacao_reduzida_lambdify
+from utils import *
 
 # Coordenadas 
 A = (3,8)
 B = (6,8)
-C = (3,12)
+C = (3,15)
 D = (10,19)
 
 distancia_ab = distancia_euclidiana(A, B)
@@ -37,9 +36,16 @@ fig, ax = plt.subplots()
 
 # Coloca os Pontos 
 ax.scatter(A[0], A[1], color='red', s=100) 
+ax.annotate(f'A', xy=(A[0], A[1]), xytext=(A[0] - 20, A[1]), textcoords='offset points', arrowprops=dict(facecolor='red', arrowstyle='->'), ha='center')
+
 ax.scatter(B[0], B[1], color='green', s=100) 
+ax.annotate(f'B', xy=(B[0], B[1]), xytext=(B[0] + 20, B[1]), textcoords='offset points', arrowprops=dict(facecolor='green', arrowstyle='->'), ha='center')
+
 ax.scatter(C[0], C[1], color='blue', s=100) 
+ax.annotate(f'C', xy=(C[0], C[1]), xytext=(C[0] - 20, C[1]), textcoords='offset points', arrowprops=dict(facecolor='blue', arrowstyle='->'), ha='center')
+
 ax.scatter(D[0], D[1], color='yellow', s=100) 
+ax.annotate(f'D', xy=(D[0], D[1]), xytext=(D[0] - 20, D[1]), textcoords='offset points', arrowprops=dict(facecolor='yellow', arrowstyle='->'), ha='center')
 
 # Coloca as distâncias entre os pontos (A e B)
 
@@ -48,15 +54,46 @@ x_vals_ab = np.linspace(min(A[0], B[0]), max(A[0], B[0]), 400)
 equacao_reduzida_func_ab = equacao_reduzida_lambdify(A, B)
 y_vals_ab = np.full_like(x_vals_ab, equacao_reduzida_func_ab(x_vals_ab))
 
-ax.plot(x_vals_ab, y_vals_ab, label=f"Distância AB {distancia_ab}")
+ax.plot(x_vals_ab, y_vals_ab, label=f"Distância AB")
 ax.set_title(distancia_ab)
-ax.set_xlabel('Tempo (s)')
-ax.set_ylabel('Posição (m)')
 
+# Entre A e C
+y_vals_ac = np.linspace(min(A[1], C[1]), max(A[1], C[1]), 400)
+equacao_reduzida_func_ac = equacao_reduzida_invertida_lambdify(A, C)
+x_vals_ac = np.full_like(y_vals_ac, equacao_reduzida_func_ac(y_vals_ac))
+
+ax.plot(x_vals_ac, y_vals_ac, label=f"Distância AC")
+ax.set_title(distancia_ac)
+
+# Entre A e D
+x_vals_ad = np.linspace(min(A[0], D[0]), max(A[0], D[0]), 400)
+equacao_reduzida_func_ad = equacao_reduzida_lambdify(A, D)
+ax.plot(x_vals_ad, equacao_reduzida_func_ad(x_vals_ad), label=f"Distância AD")
+ax.set_title(distancia_ad)
+
+# Entre B e C
+x_vals_bc = np.linspace(min(B[0], C[0]), max(B[0], C[0]), 400)
+equacao_reduzida_func_bc = equacao_reduzida_lambdify(B, C)
+print(f"{equacao_reduzida(B, C)}")
+ax.plot(x_vals_bc, equacao_reduzida_func_bc(x_vals_bc), label=f"Distância BC")
+ax.set_title(distancia_bc)
+
+# Entre B e D
+x_vals_bd = np.linspace(min(B[0], D[0]), max(B[0], D[0]), 400)
+equacao_reduzida_func_bd = equacao_reduzida_lambdify(B, D)
+ax.plot(x_vals_bd, equacao_reduzida_func_bd(x_vals_bd), label=f"Distância BD")
+ax.set_title(distancia_bd)
+
+# Entre C e D
+x_vals_cd = np.linspace(min(C[0], D[0]), max(C[0], D[0]), 400)
+equacao_reduzida_func_cd = equacao_reduzida_lambdify(C, D)
+ax.plot(x_vals_cd, equacao_reduzida_func_cd(x_vals_cd), label=f"Distância CD")
+ax.set_title(distancia_cd)
+
+# Configurar o plano cartesiano
 ax.legend()
 ax.grid(True)
 
-# Configurar o plano cartesiano
 ax.axhline(y=0, color='k')  # Eixo x
 ax.axvline(x=0, color='k')  # Eixo y
 ax.grid(True, which='both')
--- geometria_analitica/utils.py
+++ geometria_analitica/utils.py
@@ -1,27 +1,50 @@
 import sympy as sp
 
+# calcula a distância euclidiana entre 2 pontos
 def distancia_euclidiana(A, B):
     x1, y1 = sp.symbols('x1 y1')
     x2, y2 = sp.symbols('x2 y2')
-    expression = sp.sqrt(sp.Abs((x2 - x1))**2 + sp.Abs((y2 - y1))**2)
+    expression = sp.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     return expression.subs({x1: A[0], y1: B[0], x2: A[1], y2: B[1]}).evalf()
 
+# retorna a coordenada do ponto médio entre os 2 pontos
 def ponto_medio(A, B):
     return ((A[0] + B[0]) / 2, (A[1] + B[1]) / 2)
 
+# retorna a coordenada do baricentro do triângulo (centro de gravidade)
 def baricentro(A, B, C):
     return ((A[0] + B[0] + C[0]) / 3, (A[1] + B[1] + C[1]) / 3)
 
+# retorna a função python da Equação Reduzida Invertida da reta que passa pelos pontos A, B 
+# Ex: imagina a expressão 'y = 2x - 3', a sua invertida é 'x = (y - 3) / 2'
+def equacao_reduzida_invertida_lambdify(A, B):
+     return sp.lambdify(sp.symbols('y'), equacao_reduzida_invertida(A, B), 'numpy')
+
+# retorna a expressão da Equação Reduzida Invertida da reta que passa pelos pontos A, B 
+# Ex: imagina a expressão 'y = 2x - 3', a sua invertida é 'x = (y - 3) / 2'
+def equacao_reduzida_invertida(A, B):
+    coef_angular = coeficiente_angular(A, B)
+    # valida se o coeficiente angular não é 0
+    # se for, a expressão é constante, basta saber qual a constante
+    if coef_angular == 0:
+        return A[0] if A[0] == B[0] else A[1]
+    return (sp.symbols('y') - coeficiente_linear(A, B)) / coef_angular
+
+# retorna a função python da Equação Reduzida da reta que passa pelos pontos A, B 
 def equacao_reduzida_lambdify(A, B):
-    x = sp.symbols('x')
-    return sp.lambdify(x, equacao_reduzida(A, B), 'numpy')
+    return sp.lambdify(sp.symbols('x'), equacao_reduzida(A, B), 'numpy')
 
+# retorna a expressão da Equação Reduzida da reta que passa pelos pontos A, B 
 def equacao_reduzida(A, B):
-    x = sp.symbols('x')
-    return coeficiente_angular(A, B) * x + coeficiente_linear(A, B)
+    return (coeficiente_angular(A, B) * sp.symbols('x')) + coeficiente_linear(A, B)
 
+# retorna o coeficiente linear entre a reta dos 2 pontos
 def coeficiente_linear(A, B):
-    return A[1] - coeficiente_angular(A, B) * A[0]
+    return A[1] - (coeficiente_angular(A, B) * A[0])
 
+# retorna o coeficiente angular (declividade da reta) entre a reta dos 2 pontos
 def coeficiente_angular(A, B):
-    return sp.Abs((B[1] - A[1])) / sp.Abs((B[0] - A[0]))
+    diferenca_eixo_x = B[0] - A[0]
+    if diferenca_eixo_x == 0:
+        return 0
+    return (B[1] - A[1]) / diferenca_eixo_x
