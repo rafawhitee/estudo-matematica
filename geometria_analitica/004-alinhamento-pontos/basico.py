fe505fd12242a81c8e6b5164a965b713a4bb1d52
DESKTOP-VI5VDF9\rafael <rafaelnunes.inf@gmail.com>
2024-07-03 01:58:47 -0300

    no message

---
+++ geometria_analitica/004-alinhamento-pontos/basico.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Cria os 3 pontos
+A = (2, 5)
+B = (3, 9)
+C = (4, 11)
+
+# Criar a matriz 3x3
+M = np.array([
+    [A[0], A[1], 1],
+    [B[0], B[1], 1],
+    [C[0], C[1], 1]
+])
+
+# calcula o determinante da matriz
+det = np.linalg.det(M)
+print(f"Determinante: {det}")
+print(f"Colinear (estão alinhados): {det == 0}")
